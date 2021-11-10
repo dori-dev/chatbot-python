466b94495280c2f1a0da46699b7d6f90cf01487a
mohammad <mr.dori.dev@gmail.com>
2021-11-10 19:38:21 +0330

    init bot

--- smart/english_bot.py
+++ smart/english_bot.py
@@ -2,3 +2,13 @@
 """
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+
+bot = ChatBot(
+    "smart",
+    storage_adapter={
+        "import_path": "chatterbot.storage.SQLStorageAdapter",
+        "database_uri": "sqlite:///smart.db"
+    }
+)
+
+

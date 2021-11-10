76db09e4d32000013c456aea7ab3bb69a2d24d2b
mohammad <mr.dori.dev@gmail.com>
2021-11-10 19:40:30 +0330

    created bot

--- smart/english_bot.py
+++ smart/english_bot.py
@@ -13,3 +13,8 @@ bot = ChatBot(
 
 trainer = ChatterBotCorpusTrainer(bot)
 trainer.train("chatterbot.corpus.english")
+
+while True:
+    user_message = input("You> ")
+    bot_message = bot.get_response(user_message)
+    print("Bot>", bot_message)
